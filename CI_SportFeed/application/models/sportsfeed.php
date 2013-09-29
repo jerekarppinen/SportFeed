eb5dc019a25419f7ba9811714dccc16cb46994dd
jerekarppinen <jere.karppinen88@gmail.com>
2013-09-29 08:47:30 +0300

    NFL-joukkueiden linkit toimii

--- CI_SportFeed/application/models/sportsfeed.php
+++ CI_SportFeed/application/models/sportsfeed.php
@@ -98,7 +98,323 @@ class Sportsfeed extends CI_Model {
 		usort($return_array,'cmp'); 
 		
 		return $return_array; 
+	}
+
+	// Joukkuekohtaiset feedit
+	// NFL
+
+	function arizona_cardinals() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=ARZ');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+	
+	function atlanta_falcons() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=ATL');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+
+	function baltimore_ravens() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=BAL');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+
+	function buffalo_bills() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=BUF');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+
+	function carolina_panthers() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=CAR');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+	
+	function chicago_bears() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=CHI');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
 	}	
-}
+	
+	function cincinnati_bengals() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=CIN');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}	
+	
+	function cleveland_browns() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=CLV');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+	
+	function dallas_cowboys() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=DAL');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}	
+
+	function denver_broncos() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=DEN');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+	
+	function detroit_lions() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=DET');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+
+	function green_bay_packers() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=GB');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+	
+	function houston_texans() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=HOU');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}			
+	
+	function indianapolis_colts() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=IND');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+
+	function jaxonville_jaguars() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=JAX');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+
+	function kansas_city_chiefs() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=KC');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+	
+	function miami_dolphins() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=MIA');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}	
+	
+	function minnesota_vikings() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=MIN');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+
+	function new_england_patriots() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=NE');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+	
+	function new_orleans_saints() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=NO');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+	
+	function new_york_giants() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=NYG');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+	
+	function new_york_jets() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=NYJ');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+	
+	function oakland_raiders() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=OAK');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+
+	function philadelphia_eagles() {
+		
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=PHI');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}	
+	
+	function pittsburgh_stealers() {
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=PIT');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+	
+	function san_diego_chargers() {
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=SD');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+
+	function san_francisco_49ers() {
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=SF');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+
+	function seattle_seahawks() {
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=SEA');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+
+	function st_louis_rams() {
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=STL');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+
+	function tampa_bay_buccaneers() {
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=TB');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+
+	function tennessee_titans() {
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=TEN');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}
+	
+	function washington_redskins() {
+		$this->load->library('rssparser');                          // load library
+		$this->rssparser->set_feed_url('http://www.nfl.com/rss/rsslanding?searchString=team&abbr=WAS');  // get feed
+		$this->rssparser->set_cache_life(30);                       // Set cache life time in minutes
+		$rss = $this->rssparser->getFeed(10);    
+		
+		return $rss;
+	}																																												
+}									
+
 
 ?>
